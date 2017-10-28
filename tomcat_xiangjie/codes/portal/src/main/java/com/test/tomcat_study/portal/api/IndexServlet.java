271119dfab56fe1256d013522652c955b038720e
CodingWorker <dy10010@sina.cn>
2017-10-28 20:47:52 +0800

    add

---
+++ tomcat_xiangjie/codes/portal/src/main/java/com/test/tomcat_study/portal/api/IndexServlet.java
@@ -0,0 +1,11 @@
+package com.test.tomcat_study.portal.api;
+
+import javax.jws.WebService;
+
+/**
+ * Created by IntelliJ IDEA
+ * Project: tomcat-app
+ * User: DaiYan
+ * Date: 2017/10/28
+ */
+
---
+++ tomcat_xiangjie/codes/src/main/java/com/test/tomcat_study/api/IndexServlet.java
@@ -0,0 +1,25 @@
+package com.test.tomcat_study.api;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA
+ * Project: tomcat-app
+ * User: DaiYan
+ * Date: 2017/10/28
+ */
+public class IndexServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
