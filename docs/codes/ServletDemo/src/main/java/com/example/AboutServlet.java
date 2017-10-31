60f6797047ab024c4f6abdff41d9c17cdaa1a524
CodingWorker <dy10010@sina.cn>
2017-11-01 00:12:21 +0800

    ad

--- docs/codes/ServletDemo/src/main/java/com/example/AboutServlet.java
+++ docs/codes/ServletDemo/src/main/java/com/example/AboutServlet.java
@@ -1,6 +1,7 @@
 package com.example;
 
-import javax.servlet.annotation.WebServlet;
+import javax.jws.WebService;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,13 +9,13 @@ import java.io.IOException;
 import java.io.Writer;
 
 /**
- * Created by daiya on 2017/7/20.
+ * Created by daiyan on 2017/7/20.
  */
-@WebServlet
+@WebService
 public class AboutServlet extends HttpServlet{
 
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse rsp)throws IOException{
+    protected void doGet(HttpServletRequest req, HttpServletResponse rsp) throws ServletException, IOException {
         String name=req.getParameter("name");
         Writer writer=rsp.getWriter();
         writer.write("info of "+name);
